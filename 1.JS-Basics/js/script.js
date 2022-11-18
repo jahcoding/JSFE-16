35086fc6dcf7cdac4dfa2ce38a85288bb00d5e47
Ali Odiljonov <jahcoding@gmail.com>
2022-11-18 16:56:45 +0500

    J

---
+++ 1.JS-Basics/js/script.js
@@ -0,0 +1,38 @@
+// alert('hello')
+// var, let, const
+let userName
+userName = "openweb"
+
+// let brandNameSomeTest = '' //camel case
+// let brand-Name-Some-Test = '' //kebab case
+// let brand_Name_Some_Test = '' //snake case
+
+let age1 = 12 //number
+let age2 = 23.5 //number
+
+let firstname1 = "false" // string
+let count = "56" //string
+
+let bool = true //boolean
+let bool2 = false //boolean
+
+console.log(count + age1)
+//number + string
+// console.log(age1 + bool)
+//number + //boolean
+// console.log(Boolean(firstname1))
+// console.log(Boolean(age1))
+
+// falsy
+//  0
+// ""
+// false
+// NaN
+// null
+// undefined
+
+let firstname = "John"
+let lastname = "Smith"
+let age = 45
+
+console.log("Mening ismim" + " " + firstname + ", Familiyam esa " + lastname)
