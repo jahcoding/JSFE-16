a579b4124da01e5a15ed32b9d8ca0c5603b58167
Ali Odiljonov <jahcoding@gmail.com>
2022-11-21 17:10:52 +0500

    DataTypes and Operators

---
+++ 2.DataTypes/script.js
@@ -0,0 +1,61 @@
+//number
+//string
+//boolean
+//symbol
+//object
+//bigInt
+//undefined
+//null
+
+//undefined
+// let und = undefined
+let num
+console.log(num)
+
+console.log(undefined + 1)
+// undefined + number
+// console.log(typeof(NaN))
+
+//null
+let a = null
+
+console.log(a + 1)
+console.log(typeof null)
+
+// let phoneNumber = "998 90 555 55 55"
+// console.log(null === undefined)
+// console.log('5' === 5)
+// let x = 5 + 8
+// let y = x = 6
+// console.log(x)
+
+//typeof
+let num2 = 5
+
+console.log(typeof num2)
+console.log(typeof num2)
+
+let n = 56
+
+let obj = {
+	name: "Alex",
+	surname: "Smith",
+	age: n,
+	isMarried: true,
+}
+
+// obj.name = 'John'
+// obj[n] = true
+console.log(obj)
+// console.log(obj.name)
+// console.log(obj["name"])
+
+//Array
+let arr = [5, "Frontend", true, { name: "Alex" }, ["red", "blue"]]
+
+console.log(arr[3].name)
+console.log(arr[4][1])
+
+let bigint = 1234567890123456789012345678901234567890n
+
+console.log(typeof bigint)
---
+++ 3.Operators/script.js
@@ -0,0 +1,50 @@
+//Math operators
+let num = 5
+let num2 = 2
+
+// let str = "7"
+
+// console.log(num ** num2)
+// console.log(num % num2)
+// console.log(num * num2)
+// console.log(num / num2)
+// console.log(str + num)
+// console.log(str - num)
+
+//alert prompt confirm
+
+// alert("Hello Frontend")
+// prompt("Ismingizni kiriting", "Alex")
+
+// let pr = prompt("Son kiriting")
+// let pr2 = prompt("Ikkinchi sonni kiriting")
+// ''
+// console.log(Number(pr) + Number(pr2))
+// console.log(+pr + +pr2)
+// console.log(pr)
+
+let conf = confirm("Hello")
+
+console.log(conf)
+
+// EcmaScript 6 == EcmaScript 2015
+
+// console.log(a)
+// var a = 5
+
+console.log(a)
+let a = 6
+
+const BIRTHDAY = "1989.01.01"
+
+b = 7
+
+console.log(b)
+
+const obj3 = {
+	name: "Alex",
+}
+
+obj3.name = "John"
+
+console.log(obj3.name)
