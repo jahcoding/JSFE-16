ff5b2045b6fd3b8b5f4d4fc37a73e54ae9620585
Ali Odiljonov <jahcoding@gmail.com>
2022-12-21 16:44:05 +0500

    Homework

--- 11. Objects-part-2/script.js
+++ 11. Objects-part-2/script.js
@@ -1,11 +1,14 @@
 let user = {
 	name: 'John',
 	surname: 'Smith',
-	age: 45
+	age: 45,
+	colors: {
+		first: 'black',
+		second: 'pink'
+	}
 }
 
 // 1-method
-
 let admin = {}
 
 for (let key in user) {
@@ -15,6 +18,8 @@ for (let key in user) {
 // console.log(user['name'])
 
 admin.name = 'Alex'
+admin.colors.first = 'red'
+
 console.log(user)
 console.log(admin)
 
@@ -33,23 +38,20 @@ console.log(user)
 console.log(superAdmin)
 
 // 3-method
-
 user = {
 	name: 'John',
 	surname: 'Smith',
-	age: 45
+	age: 45,
+	colors: {
+		first: 'black',
+		second: 'pink'
+	}
 }
 
-let host = { 
-	...user, 
-	pet: 'Bobik',
-	sayHello: function(){
-		console.log('Hello')
-	},
-	color: 'black'
-}
+let host = { ...user, colors: {...user.colors} }
 
 host.name = 'Alex'
+host.colors.first = 'red'
 
 console.log(user)
 console.log(host)
@@ -60,3 +62,14 @@ function some( name, surname, ...smth ){
 	console.log(smth)
 }
 some(5, 6, 7, 8, 9, 0, 10, 1, 23, 35)
+
+function getCardNumber(str){
+	//code
+}
+
+getCardNumber('8600123456789101') //8600 **** **** 9101
+getCardNumber('86001234567891012') // karta xato kiritildi
+getCardNumber('860012345678910a') // karta xato kiritildi
+getCardNumber('8600123456789101') // 8600 **** **** 9101, bu uzkard
+getCardNumber('9860123456789101') // 9680 **** **** 9101, bu humo
+getCardNumber('1233123456789101') // 1233 **** **** 9101, aniqlanmadi 1 ball
